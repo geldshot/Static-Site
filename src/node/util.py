f8f9e2828ebffa39de0cb52a2e326b0d8449570e
geldshot <geldshot@gmail.com>
2025-05-08 12:04:05 -0700

    Some renaming for organizing. Added split nodes function.

--- src/node/util.py
+++ src/node/util.py
@@ -24,3 +24,15 @@ def text_node_to_html_node(text_node):
         
         case _:
             raise Exception("invalid TextType")
+        
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        splits = node.text.split(delimiter)
+        if len(splits) == 3:
+            new_nodes.append(TextNode(splits[0], node.text_type, node.url))
+            new_nodes.append(TextNode(splits[1], text_type, node.url))
+            new_nodes.append(TextNode(splits[2], node.text_type, node.url))
+        else: 
+            raise Exception("no delimiters found")
+    return new_nodes
--- tests/test_util.py
+++ tests/test_util.py
@@ -2,7 +2,7 @@ import unittest
 
 from src.node.leafnode import LeafNode
 from src.node.textnode import TextNode, TextType
-from src.node.convertnode import text_node_to_html_node
+from src.node.util import text_node_to_html_node, split_nodes_delimiter
 
 class TestTextToHTML(unittest.TestCase):
     def test_text(self):
@@ -37,3 +37,30 @@ class TestTextToHTML(unittest.TestCase):
         self.assertEqual(html_node.value, "")
         self.assertEqual(html_node.props["alt"], "This is the image")
         self.assertEqual(html_node.props["src"], "psych no image")
+
+class TestSplitDelimiter(unittest.TestCase):
+    def test_split(self):
+        node = TextNode("simple **bold** test", TextType.TEXT)
+        first = TextNode("simple ", TextType.TEXT)
+        second = TextNode("bold", TextType.BOLD)
+        third = TextNode(" test", TextType.TEXT)
+
+        nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(first, nodes[0])
+        self.assertEqual(second, nodes[1])
+        self.assertEqual(third, nodes[2])
+
+    def test_non_default(self):
+        node = TextNode("code with _italics_ in the middle", TextType.CODE)
+        first = TextNode("code with ", TextType.CODE)
+        second = TextNode("italics", TextType.ITALIC)
+        third = TextNode(" in the middle", TextType.CODE)
+
+        nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
+
+        self.assertEqual(len(nodes), 3)
+        self.assertEqual(first, nodes[0])
+        self.assertEqual(second, nodes[1])
+        self.assertEqual(third, nodes[2])
