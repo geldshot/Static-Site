c5965205d8c80c73ac99839bb14dc5e24bfd1d4a
geldshot <geldshot@gmail.com>
2025-05-10 17:49:58 -0700

    Now we are copying static into public, but we'll need to ignore public soon. Maybe static as well but that's less important.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,26 @@
+import os.path
+import os
+import shutil
 from node.textnode import *
 
 
 def main():
     txtnode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     print(txtnode)
+    if os.path.exists("./public"):
+        shutil.rmtree("./public")
+    copy_directory("./static/")
+
+def copy_directory(source, path="", destination="./public"):
+    target = destination + path
+    os.mkdir(destination +path)
+    contents = os.listdir(source + path)
+    for content in contents:
+        if os.path.isfile(source+path+"/"+content):
+            shutil.copy(source+path+"/"+content, target)
+        else:
+            copy_directory(source, path + "/" + content, destination)
+    
 
 if __name__ == "__main__":
     main()
