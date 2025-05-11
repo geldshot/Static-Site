01a7c004b6c32aeae687d7c4700eaf710e2cbc26
geldshot <geldshot@gmail.com>
2025-05-10 22:29:06 -0700

    I did not stage the images but now they are here, also we grab all the markdown from content now.

--- src/main.py
+++ src/main.py
@@ -12,18 +12,36 @@ def main():
         shutil.rmtree("./public")
     copy_directory("./static/")
 
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages("./content", destination="./public", template="./template.html")
 
 def copy_directory(source, path="", destination="./public"):
     target = destination + path
-    os.mkdir(destination +path)
+    
+    if not os.path.exists(destination+path):
+        os.mkdir(destination +path)
+    
     contents = os.listdir(source + path)
+    
     for content in contents:
         if os.path.isfile(source+path+"/"+content):
             shutil.copy(source+path+"/"+content, target)
         else:
             copy_directory(source, path + "/" + content, destination)
+
+def generate_pages(source, path="", destination="./public", template="./template.html"):
+    target = destination + path
     
+    if not os.path.exists(destination+path):
+        os.mkdir(destination +path)
+    
+    contents = os.listdir(source + path)
+    
+    for content in contents:
+        if os.path.isfile(source+path+"/"+content):
+            dest_file = content.split(".")[0] + ".html"
+            generate_page(source+path+"/"+content,template,target + "/" + dest_file)
+        else:
+            generate_pages(source, path + "/" + content, destination, template)
 
 if __name__ == "__main__":
     main()
